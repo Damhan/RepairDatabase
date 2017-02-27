cb80b4f6389f9dd61640936b71aca345a817de8a
Damhan <damhanrichardson@gmail.com>
2017-02-27 20:36:48 +0000

    Added phoneHistory which values are entered into when deleted

--- dbMain.py
+++ dbMain.py
@@ -1,15 +1,17 @@
 import sqlite3
+import datetime as d
 
 conn = sqlite3.connect("RepairDb.db")
 c = conn.cursor()
 
 #Creates an inventory table if one does not exist
 def create_table():
-    c.execute("CREATE TABLE IF NOT EXISTS phoneInventory(phoneId INTEGER, phoneBrand TEXT, phoneModel TEXT, buyPrice INTEGER, repairCost INTEGER, saleTarget INTEGER)")
+	c.execute("CREATE TABLE IF NOT EXISTS phoneInventory(phoneId INTEGER, phoneBrand TEXT, phoneModel TEXT, buyPrice INTEGER, repairCost INTEGER, saleTarget INTEGER)")
+	c.execute("CREATE TABLE IF NOT EXISTS phoneHistory(phoneId INTEGER, date TEXT,buyP INTEGER,repairP INTEGER,sellP INTEGER,profit INTEGER)")
 
 
 def dynamic_data_entry():
-    #Get the input and sort it into respective variables
+    #Get the input and sort it into respective variables (DOESNT MAKE A CHECK FOR INVALID VALUES!!)
     print("Enter values to be inserted:")
     inputline = input()
     items = inputline.split()
@@ -24,14 +26,8 @@ def dynamic_data_entry():
     (phoneId,phoneBrand,phoneModel,buyPrice,repairCost,saleTarget))
 
     conn.commit()
-#returns everything in a table and prints it
-def get_table(t):
-    c.execute("SELECT * FROM {} ".format(t))
-    data = c.fetchall()
-    for d in data:
-        print(d)
 
-#Handles inserting values into tables (currently only one static table)
+#Handles inserting values into tables (ONLY HANDLES INSERTION INTO phoneInventory!!) 
 def inventory_insertion():
     print("Press enter to begin inserting values.")
     print("Please enter values in the form 'phoneId(int) phoneBrand(str) phonemodel(str) buyPrice(int) repairCost(int) saleTarget(int)'")
@@ -41,7 +37,7 @@ def inventory_insertion():
         print("When you are done inputting values, please enter 'done', otherwise press enter")
         inputting = input()
 
-
+#handles deleting values from phoneInventory (DOESNT HANDLE OTHER TABLES!!)
 def inventory_deletion():
 	print("Do you need to see the table first? y/n")
 	answer = input()
@@ -49,7 +45,28 @@ def inventory_deletion():
 		get_table("phoneInventory")
 	print("Enter the id you wish to delete:")
 	idPick = input()
-	c.execute("DELETE FROM phoneInventory WHERE phoneID = {}".format(idPick))
+	c.execute("SELECT * FROM phoneInventory WHERE phoneId= ?",(idPick,))
+	phoneInfo = c.fetchall()
+	c.execute("DELETE FROM phoneInventory WHERE phoneId = ?",(idPick,))
+	conn.commit()
+	phone_history_insertion(phoneInfo)
+
+#Takes info from phoneInventory on deletion and inserts it into the phoneHistory table
+def phone_history_insertion(phoneInfo):
+	#set up the variables to pass
+	phoneInfo = phoneInfo[0]
+	phoneId = phoneInfo[0]
+	date = d.datetime.today()
+	print(phoneInfo)
+	buyP = phoneInfo[3]
+	repairP = phoneInfo[4]
+	sellP = phoneInfo[5]
+	profit = (sellP-buyP) - repairP
+	#insert the values into a new row
+	c.execute("INSERT INTO phoneHistory (phoneId, date,buyP,repairP,sellP,profit)  VALUES (?,?,?,?,?,?)",
+	(phoneId,date,buyP,repairP,sellP,profit))
+	print("Adding to history...")
+	print("Added!")
 
 
 #Handles viewing tables
@@ -57,6 +74,14 @@ def table_viewing():
     print("Enter the name of the table you would like to view")
     tableChoice = input()
     get_table(tableChoice)
+
+#returns everything in a table and prints it
+def get_table(t):
+    c.execute("SELECT * FROM {} ".format(t))
+    data = c.fetchall()
+    for d in data:
+        print(d)
+
 
 #main functions
 def main():
